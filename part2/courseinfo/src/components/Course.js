520f8810f89d41a8cb1321b4b4e9c5e9de726741
Janne Immonen <91911462+JanneImmonen@users.noreply.github.com>
2023-07-04 19:59:52 +0300

    Create Course.js

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      {course.parts.map(part =>
+        <p key={part.id}>
+          {part.name} {part.exercises}
+        </p>
+      )}
+      <b>total of {course.parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</b>
+    </div>
+  )
+}
+
+export default Course;
